58cb106dcb73f1d8edd307a5dde8dd5e57001265
ShubhamShuklaX <shubhshukla031@gmail.com>
2025-11-13 11:48:54 +0530

    fixed no token issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,5 +5,15 @@ export const axiosInstance = axios.create({
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
       : `${import.meta.env.VITE_API_BASE_URL}/api`,
-  withCredentials: true,
+});
+
+// Attach JWT token for every request
+axiosInstance.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
 });
